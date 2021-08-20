52ad1c06d84776361cbfee7f3db46ce84d7ccb1a
MardonioMelo <mardonio.quimico@gmail.com>
2021-08-19 21:05:36 -0300

    remoção do arquivo main.js no controle de versão

--- example.main.js
+++ example.main.js
@@ -4,9 +4,9 @@
 */
 
 sessionStorage.setItem("my_setup", JSON.stringify({
-    "uuid": "b18c5dc2-0b2c-4a9a-be53-48beb8743e82",
-    "type": "attendant",
-    "public": "ffc6wwq2eb25f5asasf11a7f1b7546cb3ca",
-    "host_http": "http://localhost:81/chat_api/api",
-    "host_ws": "localhost:8081"
+    "uuid": "",
+    "type": "",
+    "public": "",
+    "host_http": "",
+    "host_ws": ""
 }))
